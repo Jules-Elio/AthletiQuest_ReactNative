0685833ab49f955f363eb0783028cc7fddccb2b4
Elio Darras <elio.darras_ext@sonepar.com>
2025-04-30 16:31:55 +0200

    add fictif test e2e

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'jest-expo',
   testMatch: ['<rootDir>/e2e/**/*.test.ts?(x)'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
     'node_modules/(?!(expo-modules-core|expo(nent)?|@expo(nent)?/.*|react-native.*|@react-native.*))',
